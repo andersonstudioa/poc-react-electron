071b4a47da74557e97c5961f3cff9a305a0b031d
andersonstudioa <anderson@studioaweb.com.br>
2024-09-02 20:47:24 -0300

    Configuração full screen no electron

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-// main.js
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow } from "electron";
+//import { join } from "path";
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1280,
-    height: 600,
+    show: false
   });
-
-  win.loadURL('http://127.0.0.1:5173');
+  win.maximize();
+  win.loadURL("http://localhost:5173");
+  win.show();
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.on("active", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+app.on("window-all-closed", () => {
+  // eslint-disable-next-line no-undef
+  if (process.platform !== "darwin") app.quit();
 });
