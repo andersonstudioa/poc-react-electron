c5caf53e6564db641ad8c1ff76524843a6640625
andersonstudioa <anderson@studioaweb.com.br>
2024-09-03 14:51:34 -0300

    Configurações de build

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
-import { app, BrowserWindow } from "electron";
-//import { join } from "path";
+/* eslint-disable no-undef */
+import { app, BrowserWindow } from 'electron';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Resolver __dirname em módulos ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
-    show: false
+    width: 800,
+    height: 600,
+    fullscreen: true, // Inicia em fullscreen
+    autoHideMenuBar: true, // Oculta a barra de menus
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // Adiciona o preload script
+      nodeIntegration: true, // Desabilitar a integração com Node.js para segurança
+      contextIsolation: false, // Manter o isolamento de contexto
+    },
   });
-  win.maximize();
-  win.loadURL("http://localhost:5173");
+
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173'); // Durante o desenvolvimento
+  } else {
+    win.loadFile(path.join(__dirname, 'dist/index.html')); // No modo de produção
+  }
+
   win.show();
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on("active", () => {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-app.on("window-all-closed", () => {
-  // eslint-disable-next-line no-undef
-  if (process.platform !== "darwin") app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
